e61842120c5f136278378553d78897370f4a8151
BadLagger <lanynamer1985@gmail.com>
2025-10-22 22:22:25 +0300

    feat: подняты все три сервиса, всё работает через gateway!

---
+++ hotel-service/src/main/java/org/mifistudy/grechko/hotelservice/controller/HotelController.java
@@ -0,0 +1,15 @@
+package org.mifistudy.grechko.hotelservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hotels")
+public class HotelController {
+
+    @GetMapping("/status")
+    public String status() {
+        return "Hotel Service is UP!";
+    }
+}
