69f6038da8bb368236d38480565ac95970dcb04c
BadLagger <lanynamer1985@gmail.com>
2026-01-13 04:10:46 +0300

    Add security, database and lombok dependencies; create user entity

---
+++ booking-service/src/main/java/org/mifistudy/grechko/bookingservice/entity/User.java
@@ -0,0 +1,40 @@
+package com.hotel.booking.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    public enum Role {
+        USER,
+        ADMIN
+    }
+}
